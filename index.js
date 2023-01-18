1aee9c3fa402048a2a410181160f7e0b6c7adf5c
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-18 18:04:20 +0200

    basic route structure created

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const shelterRoutes = require('./routes/shelters');
+const dogRoutes = require('./routes/dogs');
+
+app.use('/shelters', shelterRoutes);
+app.use('/dogs', dogRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
