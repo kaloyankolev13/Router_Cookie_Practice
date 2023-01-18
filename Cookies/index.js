b76815300dcd510fa8ac391d500ab52184cfb126
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-18 23:56:31 +0200

    sending cookies

--- Cookies/index.js
+++ Cookies/index.js
@@ -1,5 +1,15 @@
-const express = require('express');
-const app = express;
+const express = require('express')();
+let app = express;
+
+app.get('/greet', (req, res) => {
+  res.send('Hey there');
+});
+
+app.get('/setname', (req, res) => {
+  res.cookie('name', 'stevie wonder');
+  res.cookie('animal', 'harlequin shrimp');
+  res.send('OK sent you a cookie');
+});
 
 app.listen(3000, () => {
   console.log('SERVING');
