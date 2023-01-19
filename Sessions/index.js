e5c9ed762c33e1220248acea4ee28f0439cfc486
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-19 11:32:07 +0200

    more practice with sessions

--- Sessions/index.js
+++ Sessions/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 let session = require('express-session');
 
-app.use(session({ secret: 'thisismysecret' }));
+const sessionOptons = {
+  secret: 'thisismysecret',
+  resave: false,
+  saveUninitialized: false,
+};
+
+app.use(session(sessionOptons));
 
 app.get('/viewcount', (req, res) => {
   if (req.session.count) {
@@ -11,6 +17,17 @@ app.get('/viewcount', (req, res) => {
     req.session.count = 1;
   }
   res.send(`You have viewed this page ${req.session.count} times`);
+});
+
+app.get('/register', (req, res) => {
+  const { username = 'unknown' } = req.query;
+  req.session.username = username;
+  res.redirect('/greet');
+});
+
+app.get('/greet', (req, res) => {
+  const { username } = req.session;
+  res.send(`Welcome back, ${username}`);
 });
 
 app.listen(3000, () => {
