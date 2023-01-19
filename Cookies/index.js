bf372775ef0aecfc8c68a52a38e6cc8f397d1d08
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-19 10:02:30 +0200

    added a cookie-parser middleware and tested it out

--- Cookies/index.js
+++ Cookies/index.js
@@ -1,8 +1,12 @@
 const express = require('express')();
 let app = express;
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.get('/greet', (req, res) => {
-  res.send('Hey there');
+  const { name = 'Anonymus' } = req.cookies;
+  res.send(`Hey there ${name}`);
 });
 
 app.get('/setname', (req, res) => {
