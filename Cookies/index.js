2a64f1fe308fdc3dc140f8b020b12216380c28c6
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-01-19 10:34:19 +0200

    added signed cookies

--- Cookies/index.js
+++ Cookies/index.js
@@ -2,7 +2,7 @@ const express = require('express')();
 let app = express;
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req, res) => {
   const { name = 'Anonymus' } = req.cookies;
@@ -13,6 +13,16 @@ app.get('/setname', (req, res) => {
   res.cookie('name', 'stevie wonder');
   res.cookie('animal', 'harlequin shrimp');
   res.send('OK sent you a cookie');
+});
+
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'grape', { signed: true });
+  res.send('Signed your cookie');
+});
+
+app.get('/verifyfruit', (req, res) => {
+  console.log(req.cookies);
+  res.send(req.signedCookies);
 });
 
 app.listen(3000, () => {
